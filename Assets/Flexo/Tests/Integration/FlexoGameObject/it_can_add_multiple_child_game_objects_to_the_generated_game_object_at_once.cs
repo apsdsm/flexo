67b20607ef22a7a8575626198542466542de1600
Nick del Pozo <nickdelpozo@gmail.com>
2015-12-19 16:14:24 +0900

    tests for new functionality

---
+++ Assets/Flexo/Tests/Integration/FlexoGameObject/it_can_add_multiple_child_game_objects_to_the_generated_game_object_at_once.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Flexo.Test
+{
+
+    [IntegrationTest.DynamicTest( "Flexo.FlexoGameObjectTests" )]
+    public class it_can_add_multiple_child_game_objects_to_the_generated_game_object_at_once : MonoBehaviour
+    {
+
+        FlexoGameObject flexo;
+
+        // setup
+        void Awake ()
+        {
+            flexo = new FlexoGameObject( "Foo" ).WithChildren( "Bar", "Baz", "Boz" );
+        }
+
+        // test
+        void Update ()
+        {
+            int childCount = flexo.GameObject.transform.childCount;
+
+            IntegrationTest.Assert( childCount == 3, "Expected exactly 3 children, but found: " + childCount.ToString() );
+            IntegrationTest.Pass();
+        }
+
+        // teardown
+        void OnDisable ()
+        {
+        }
+    }
+}
---
+++ Assets/Flexo/Tests/Integration/FlexoGameObject/it_can_provide_references_to_components_as_they_are_added_using__has__method.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Flexo.Test
+{
+    [IntegrationTest.DynamicTest( "Flexo.FlexoGameObjectTests" )]
+    public class it_can_provide_references_to_components_as_they_are_added_using__has__method : MonoBehaviour
+    {
+
+        FlexoGameObject flexo;
+        TestComponent component;
+
+        // setup
+        void Awake ()
+        {
+            flexo = new FlexoGameObject( "Foo" ).Has<TestComponent>( out component );
+        }
+
+        // test
+        void Update ()
+        {
+            GameObject focused = flexo.FocusedGameObject;
+
+            IntegrationTest.Assert( component != null, "Expected component to reference a component, it was null" );
+            IntegrationTest.Pass();
+        }
+
+        // teardown
+        void OnDisable ()
+        {
+        }
+    }
+}
